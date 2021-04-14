2865e47a23542720f74b221fd5349f563ef5ef3c
MooseValley <Moose_Software@yahoo.com.au>
2021-04-14 15:40:06 +1000

    v0.02
    v0.02 - improve formatting for code, output and README

--- EulerSquares.java
+++ EulerSquares.java
@@ -2,40 +2,13 @@
 https://www.youtube.com/watch?v=qu04xLNrk94
 Euler Squares - Numberphile
 
-Graeco-Latin Squares
-Double Latin Sqaures
+Euler Squares, aka Double Latin Squares, aka Graeco-Latin Squares
 
 2x2 cannot be done.
 6x6 cannot be done.
 
 *** Sample output:
-
-Group 3x3:
-A-1  B-2  C-3
-C-2  A-3  B-1
-B-3  C-1  A-2
-
-Teams 4x4:
-Mike vs Murray      Frankie vs Hank     Bella vs Saminal    Teenie vs Jessica
-Teenie vs Hank      Mike vs Saminal     Frankie vs Jessica  Bella vs Murray
-Bella vs Saminal    Teenie vs Jessica   Mike vs Murray      Frankie vs Hank
-Frankie vs Jessica  Bella vs Murray     Teenie vs Hank      Mike vs Saminal
-
-Group 5x5:
-A-1  B-2  C-3  D-4  E-5
-E-2  A-3  B-4  C-5  D-1
-D-3  E-4  A-5  B-1  C-2
-C-4  D-5  E-1  A-2  B-3
-B-5  C-1  D-2  E-3  A-4
-
-Group 6x6:
-A-1  B-2  C-3  D-4  E-5  F-6
-F-2  A-3  B-4  C-5  D-6  E-1
-E-3  F-4  A-5  B-6  C-1  D-2
-D-4  E-5  F-6  A-1  B-2  C-3
-C-5  D-6  E-1  F-2  A-3  B-4
-B-6  C-1  D-2  E-3  F-4  A-5
-ERROR: does not work - the same combinations appear multiple times.
+See README.MD
 
 */
 public class EulerSquares
